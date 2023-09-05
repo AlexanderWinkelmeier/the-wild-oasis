3de4a03ff0cb2bd189370ad79c26d6e9f655271b
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2023-09-05 07:24:51 +0200

    Uploading Images to Supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://npkbzmbbycmbbavnpptp.supabase.co';
+export const supabaseUrl = 'https://npkbzmbbycmbbavnpptp.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5wa2J6bWJieWNtYmJhdm5wcHRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMzMDIzNTIsImV4cCI6MjAwODg3ODM1Mn0.elzPs3uY9SMFKdTqA41W9TjJmMCJ5n6oy4DJrQf70JU';
 const supabase = createClient(supabaseUrl, supabaseKey);
