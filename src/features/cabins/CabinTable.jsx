e7590ec874d9a26b6236f2729672507b599af7f2
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2023-09-17 06:34:22 +0200

    Building Resusable Context Menu

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable react/prop-types */
 import { useFetchCabins } from './useFetchCabins.js';
 import styled from 'styled-components';
 
 import Spinner from '../../ui/Spinner.jsx';
 import CabinRow from './CabinRow.jsx';
 import Table from '../../ui/Table.jsx';
+import Menus from '../../ui/Menus.jsx';
 
 const TableHeader = styled.header`
   display: grid;
@@ -26,20 +28,22 @@ function CabinTable() {
   if (isLoading) return <Spinner />;
 
   return (
-    <Table colums="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
-      <Table.Header>
-        <div></div>
-        <div>Cabin</div>
-        <div>Capacity</div>
-        <div>Price</div>
-        <div>Discount</div>
-        <div></div>
-      </Table.Header>
-      <Table.Body
-        data={cabins}
-        render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
-      />
-    </Table>
+    <Menus>
+      <Table colums="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div></div>
+          <div>Cabin</div>
+          <div>Capacity</div>
+          <div>Price</div>
+          <div>Discount</div>
+          <div></div>
+        </Table.Header>
+        <Table.Body
+          data={cabins}
+          render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+        />
+      </Table>
+    </Menus>
   );
 }
 
