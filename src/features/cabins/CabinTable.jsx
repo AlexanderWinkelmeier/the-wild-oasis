91f8095003d59e71688cdddddd12f7b65c57ff40
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2023-09-18 07:03:19 +0200

    Client-Side Filtering: Filtering Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,31 +1,27 @@
 /* eslint-disable react/prop-types */
 import { useFetchCabins } from './useFetchCabins.js';
-import styled from 'styled-components';
 
 import Spinner from '../../ui/Spinner.jsx';
 import CabinRow from './CabinRow.jsx';
 import Table from '../../ui/Table.jsx';
 import Menus from '../../ui/Menus.jsx';
-
-const TableHeader = styled.header`
-  display: grid;
-  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-  column-gap: 2.4rem;
-  align-items: center;
-
-  background-color: var(--color-grey-50);
-  border-bottom: 1px solid var(--color-grey-100);
-  text-transform: uppercase;
-  letter-spacing: 0.4px;
-  font-weight: 600;
-  color: var(--color-grey-600);
-  padding: 1.6rem 2.4rem;
-`;
+import { useSearchParams } from 'react-router-dom';
 
 function CabinTable() {
   const { isLoading, cabins } = useFetchCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get('discount') || 'all';
+  // ruft die jeweiligen searchParams von "discount" in der URL ab; wenn nicht vorhanden, d.h. null, dann wird "all" gesetzt; das passiert beim Mounting der Komponenten, wenn noch kein Filter-Button gedrückt wurde
+
+  let filteredCabins;
+  if (filterValue === 'all') filteredCabins = cabins;
+  if (filterValue === 'no-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.disount === 0);
+  if (filterValue === 'with-discount')
+    filteredCabins = cabins.filter((cabin) => cabin.disount > 0);
 
   return (
     <Menus>
@@ -39,7 +35,7 @@ function CabinTable() {
           <div></div>
         </Table.Header>
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
